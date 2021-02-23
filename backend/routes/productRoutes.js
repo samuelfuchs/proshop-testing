260f61f6d80b2f76822b7b98e398dbeeb6be7ea6
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-23 17:00:46 -0300

    custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -22,7 +22,8 @@ router.get('/:id', asyncHandler(async (req, res) => {
     if (product) {
         res.json(product)
     } else {
-        res.status(404).json({ message: 'Product not found' })
+        res.status(404)
+        throw new Error('Product not found')
     }
 }))
 
