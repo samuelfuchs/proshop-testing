4b70e28cbfacd55e1be3bab6c92893cea02818ff
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-26 17:28:58 -0300

    Message & Loader Components

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router()
 // @access      Public
 router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({})
-
+    // throw new Error('Some error')
     res.json(products)
 }))
 
