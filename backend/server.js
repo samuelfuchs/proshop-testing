5e4bf99d15461ab430fed8a9bc81bd05a99bcad9
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-27 23:23:54 -0300

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 
 dotenv.config()
@@ -12,11 +14,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send("API is running....")
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
