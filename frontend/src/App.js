aa1bd7c566c203110d822bfa5b14e5c29fb59358
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-01 23:31:53 -0300

    Profile Screen & Get User Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,8 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
-//more comments added
+import ProfileScreen from './screens/ProfileScreen'
+
 const App = () => {
   return (
     <Router>
@@ -16,6 +17,7 @@ const App = () => {
           <Container>
             <Route path='/login' component={LoginScreen} />
             <Route path='/register' component={RegisterScreen} />
+            <Route path='/profile' component={ProfileScreen} />
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
             <Route path='/' component={HomeScreen} exact />
