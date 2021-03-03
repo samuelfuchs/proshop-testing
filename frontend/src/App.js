6c9dde99f66a99f8b7e026e5befbfc98a1bce0f2
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-03 16:45:36 -0300

    Payment Screen & Save Payment Method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <main className='py-3'>
           <Container>
             <Route path='/login' component={LoginScreen} />
+            <Route path='/payment' component={PaymentScreen} />
             <Route path='/shipping' component={ShippingScreen} />
             <Route path='/register' component={RegisterScreen} />
             <Route path='/profile' component={ProfileScreen} />
