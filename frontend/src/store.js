b016099ea2a88dff67a6a1abfba3be15e42d392c
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-05 15:31:27 -0300

    Update User Functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,6 +10,7 @@ import {
     userUpdateProfileReducer,
     userListReducer,
     userDeleteReducer,
+    userUpdateReducer
 } from './reducers/userReducers'
 
 import { 
@@ -29,6 +30,7 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
     userList: userListReducer,
     userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
