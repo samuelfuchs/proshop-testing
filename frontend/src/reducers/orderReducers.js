d75db1f1d9af76a6db6d405b1c712bbb050c1f52
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-04 22:58:13 -0300

    Order Pay Reducer & Action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -4,7 +4,11 @@ import {
     ORDER_CREATE_SUCCESS,
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_FAIL,
-    ORDER_DETAILS_SUCCESS
+    ORDER_DETAILS_SUCCESS,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_RESET
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -46,6 +50,29 @@ export const orderDetailsReducer = (state = { loading: true, orderItems: [], shi
                 loading: false,
                 error: action.payload,
             }
+        default:
+            return state
+    }
+}
+
+export const orderPayReducer = (state = {}, action) => {
+    switch(action.type) {
+        case ORDER_PAY_REQUEST:
+            return {
+                loading: true
+            }
+        case ORDER_PAY_SUCCESS:
+            return {
+                loading: false,
+                success: true
+            }
+        case ORDER_PAY_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            }
+        case ORDER_PAY_RESET:
+            return {}
         default:
             return state
     }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,7 +10,11 @@ import {
     userUpdateProfileReducer,
 } from './reducers/userReducers'
 
-import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers'
+import { 
+    orderCreateReducer, 
+    orderDetailsReducer,
+    orderPayReducer
+} from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -22,6 +26,7 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
