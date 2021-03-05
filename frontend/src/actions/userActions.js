694b45e9226c965de47aca843bbd4aa05dcc17a4
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-05 04:26:59 -0300

    Admin Screen Access Security

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,7 +16,8 @@ import {
     USER_DETAILS_RESET,
     USER_LIST_REQUEST,
     USER_LIST_SUCCESS,
-    USER_LIST_FAIL
+    USER_LIST_FAIL,
+    USER_LIST_RESET
 } from "../constants/userConstants"
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
@@ -59,6 +60,7 @@ export const logout = () => (dispatch) => {
     dispatch({ type: USER_LOGOUT })
     dispatch({ type: USER_DETAILS_REQUEST })
     dispatch({ type: ORDER_LIST_MY_RESET })
+    dispatch({ type: USER_LIST_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
