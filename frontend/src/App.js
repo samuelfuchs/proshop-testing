c4c5bff8257e1da678114bfac71fe2b3551341ad
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-05 16:54:24 -0300

    Admin Product List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 const App = () => {
   return (
@@ -31,6 +32,7 @@ const App = () => {
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
             <Route path='/admin/userlist' component={UserListScreen} />
+            <Route path='/admin/productlist' component={ProductListScreen} />
             <Route path='/admin/user/:id/edit' component={UserEditScreen} />
             <Route path='/' component={HomeScreen} exact />
           </Container>
