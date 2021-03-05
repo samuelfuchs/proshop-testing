2a5c3c8f9038cdff0ad1a18cd93ddff30934ceb8
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-05 03:31:03 -0300

    User Details & Orders Reset

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -12,8 +12,10 @@ import {
     USER_REGISTER_SUCCESS, 
     USER_UPDATE_PROFILE_FAIL, 
     USER_UPDATE_PROFILE_REQUEST,
-    USER_UPDATE_PROFILE_SUCCESS
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_DETAILS_RESET
 } from "../constants/userConstants"
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -52,6 +54,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({ type: USER_LOGOUT })
+    dispatch({ type: USER_DETAILS_REQUEST })
+    dispatch({ type: ORDER_LIST_MY_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -11,7 +11,8 @@ import {
     ORDER_PAY_RESET,
     ORDER_LIST_MY_FAIL,
     ORDER_LIST_MY_REQUEST,
-    ORDER_LIST_MY_SUCCESS
+    ORDER_LIST_MY_SUCCESS,
+    ORDER_LIST_MY_RESET
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -96,6 +97,10 @@ export const orderListMyReducer = (state = { orders: []}, action) => {
             return {
                 loading: false,
                 error: action.payload,
+            }
+        case ORDER_LIST_MY_RESET:
+            return {
+                order: []
             }
         default:
             return state
