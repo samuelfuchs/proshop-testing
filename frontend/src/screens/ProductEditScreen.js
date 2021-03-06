c362b16927eff4b3d77177ee2fd4c2197de01d85
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-06 19:18:48 -0300

    Admin Update Product

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { listProductDetails } from '../actions/productActions'
+import { listProductDetails, updateProduct } from '../actions/productActions'
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
 
 const ProductEditScreen = ({ match, history }) => {
     const productId = match.params.id
@@ -23,8 +24,14 @@ const ProductEditScreen = ({ match, history }) => {
     const productDetails = useSelector(state => state.productDetails)
     const { loading, error, product } = productDetails
 
+    const productUpdate = useSelector(state => state.productUpdate)
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = productUpdate
+
     useEffect(() => {
-        
+        if(successUpdate) {
+            dispatch({ type: PRODUCT_UPDATE_RESET })
+            history.push('/admin/productlist')
+        } else {
             if(!product.name || product._id !== productId) {
                 dispatch(listProductDetails(productId))
             } else {
@@ -36,11 +43,22 @@ const ProductEditScreen = ({ match, history }) => {
                 setCountInStock(product.countInStock)
                 setDescription(product.description)
             }
-    }, [dispatch, history, productId, product])
+        }
+        
+    }, [dispatch, history, productId, product, successUpdate])
 
     const submitHandler = (e) => {
         e.preventDefault()
-        //update product
+        dispatch(updateProduct({
+            _id: productId,
+            name,
+            price,
+            brand,
+            image,
+            category,
+            description,
+            countInStock
+        }))
     }
     return (
         <>
@@ -49,6 +67,8 @@ const ProductEditScreen = ({ match, history }) => {
             </Link>
             <FormContainer>
             <h1>Edit Product</h1>
+            {loadingUpdate && <Loader />}
+            {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                 <Form onSubmit={submitHandler}>
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,8 @@ import {
     productListReducer, 
     productDetailsReducer,
     productDeleteReducer,
-    productCreateReducer
+    productCreateReducer,
+    productUpdateReducer
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { 
@@ -30,6 +31,7 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     productDelete: productDeleteReducer,
     productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
