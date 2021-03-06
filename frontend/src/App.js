005ee4cb27645dafb7a06ff04c9da64dd3a2cee0
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-06 15:20:55 -0300

    Edit Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
 
 const App = () => {
   return (
@@ -32,8 +33,9 @@ const App = () => {
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
             <Route path='/admin/userlist' component={UserListScreen} />
-            <Route path='/admin/productlist' component={ProductListScreen} />
             <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+            <Route path='/admin/productlist' component={ProductListScreen} />
+            <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
             <Route path='/' component={HomeScreen} exact />
           </Container>
         </main>
