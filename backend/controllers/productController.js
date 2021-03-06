0640ea9e859294a27d3defacdfadac9f766024cb
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-06 07:54:55 -0300

    Admin Delete Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,7 +24,24 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 })
 
+
+// @desc        Delete a product
+// @route       DELETE /api/products/:id
+// @access      Private/admin
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product: removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
 export {
     getProducts,
-    getProductById
+    getProductById,
+    deleteProduct
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { 
+    productListReducer, 
+    productDetailsReducer,
+    productDeleteReducer
+} from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { 
     userLoginReducer, 
@@ -23,6 +27,7 @@ import {
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
