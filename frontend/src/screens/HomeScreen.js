83fd57af343166a371912bdbaec8978d7cf38642
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-08 14:15:06 -0300

    Product Pagination

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
 import { listProducts } from '../actions/productActions'
 
 
@@ -16,7 +17,7 @@ const HomeScreen = ({ match }) => {
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+    const { loading, error, products, page, pages } = productList
 
     useEffect(() => {
         dispatch(listProducts(keyword, pageNumber))
@@ -30,13 +31,21 @@ const HomeScreen = ({ match }) => {
             ) : error ? (
                 <Message variant='danger'>{error}</Message>
             ) : (
+                <>
                 <Row>
                     {products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                             <Product product={product} />
                         </Col>
                     ))}
-                </Row>)}
+                </Row>
+                <Paginate 
+                    pages={pages} 
+                    page={page} 
+                    keyword={keyword ? keyword : ''} 
+                />
+                </>
+                )}
             
         </>
     )
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,14 +4,17 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
 import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
 import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = ({ history, match }) => {
+  const pageNumber = match.params.pageNumber || 1
+
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+    const { loading, error, products, page, pages } = productList
 
     const productDelete = useSelector(state => state.productDelete)
     const { 
@@ -40,9 +43,17 @@ const ProductListScreen = ({ history, match }) => {
         if(successCreate) {
           history.push(`/admin/product/${createdProduct._id}/edit`)
         } else {
-          dispatch(listProducts())
+          dispatch(listProducts('', pageNumber))
         }
-    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
+    }, [
+        dispatch, 
+        history, 
+        userInfo, 
+        successDelete, 
+        successCreate, 
+        createdProduct,
+        pageNumber
+      ])
 
     const deleteHandler = (id) => {
       if(window.confirm('Are you sure')) {
@@ -76,6 +87,7 @@ const ProductListScreen = ({ history, match }) => {
           ) : error ? (
             <Message variant='danger'>{error}</Message>
           ) : (
+            <>
             <Table striped bordered hover responsive className='table-sm'>
               <thead>
                 <tr>
@@ -119,6 +131,8 @@ const ProductListScreen = ({ history, match }) => {
                 ))}
               </tbody>
             </Table>
+            <Paginate pages={pages} page={page} isAdmin={true} />
+            </>
           )}
         </>
       )
